f731af9f268ca63a346a9c44f8a4480bb4edfdf1
EdsJDev <edson-ramos54@hotmail.com>
2025-09-08 17:38:25 -0300

    added button to back for initial screen, resizing layout PreencherDadosActivity, added function for open PreencherDadosActivity

--- app/src/main/java/com/example/logimeta/MainActivity.kt
+++ app/src/main/java/com/example/logimeta/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.logimeta
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn_NovoTeste: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btn_NovoTeste = findViewById(R.id.btn_NovoTeste)
+
+
+        btn_NovoTeste.setOnClickListener {
+            val intent = Intent(this, PreencherDadosActivity::class.java)
+            startActivity(intent)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/logimeta/PreencherDadosActivity.kt
+++ app/src/main/java/com/example/logimeta/PreencherDadosActivity.kt
@@ -1,16 +1,28 @@
 package com.example.logimeta
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PreencherDadosActivity : AppCompatActivity() {
+
+    lateinit var voltar_imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_preencher_dados)
+
+        voltar_imageView = findViewById(R.id.voltar_imageView)
+
+        voltar_imageView.setOnClickListener {
+            finish()
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

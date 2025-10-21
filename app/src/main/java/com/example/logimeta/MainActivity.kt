5613d6c13f1e24f9517af5df7337159ca52e2bdc
EdsJDev <edson-ramos54@hotmail.com>
2025-10-21 17:58:08 -0300

    removed unsued code

--- app/src/main/java/com/example/logimeta/MainActivity.kt
+++ app/src/main/java/com/example/logimeta/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.logimeta
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,14 +13,6 @@ import kotlin.getValue
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btn_NovoTeste: Button
-    lateinit var historico_button: Button
-    lateinit var media_geral_button: Button
-
-    private val bancoDeDados by lazy {
-        DatabaseHelper(this)
-    }
-
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -31,47 +22,17 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
-//        // teste para verificar se o banco de dados está funcionando
-//        try {
-//            bancoDeDados.writableDatabase.execSQL(
-//                "INSERT INTO SessaoColeta (" +
-//                        "    nome_separador," +
-//                        "    modulo_selecionado," +
-//                        "    total_enderecos," +
-//                        "    total_itens" +
-//                        ")" +
-//                        "VALUES (" +
-//                        "    'Alice Silva'," +
-//                        "    'Mod A - Picking Lento'," +
-//                        "    25," +
-//                        "    150" +
-//                        ");"
-//            )
-//            Log.i("info_db", "Dados salvos com sucesso")
-//
-//
-//        }catch (e: Exception){
-//            e.printStackTrace()
-//        }
-//        // fim do teste
-
-        btn_NovoTeste = findViewById(R.id.btn_NovoTeste)
-        historico_button = findViewById(R.id.historico_button)
-        media_geral_button = findViewById(R.id.media_geral_button)
-
-
-
-        historico_button.setOnClickListener {
+        binding.historicoButton.setOnClickListener {
             val intent = Intent(this, HistoricoDeTestesActivity::class.java)
             startActivity(intent)
         }
 
-        media_geral_button.setOnClickListener {
+        binding.mediaGeralButton.setOnClickListener {
             val intent = Intent(this, EscolherModuloActivity::class.java)
             startActivity(intent)
         }
 
-        btn_NovoTeste.setOnClickListener {
+        binding.btnNovoTeste.setOnClickListener {
             val intent = Intent(this, PreencherDadosActivity::class.java)
             startActivity(intent)
         }
